882e9df4b26e0db5bf43346b78a27d8c614be4a2
adi wirya <50317150+adiwirya7@users.noreply.github.com>
2020-07-28 01:37:48 +0700

    Quiz 1

---
+++ Pekan 1/Quiz Pekan 1/quiz.js
@@ -0,0 +1,46 @@
+// soal 2
+console.log('Soal 2');
+function luasSegitiga(a,t) {
+    return a*t/2;
+}
+function luasLingkaran(r) {
+    return Math.PI*r*r
+}
+function luasPersegi(s) {
+    return s*s
+}
+console.log(luasSegitiga(10,5));
+console.log(luasPersegi(15));
+console.log(luasLingkaran(7));
+
+// // soal 3
+// console.log('Soal 3');
+// index=0;
+// var daftarAlatTulis = ["2. Pensil", "5. Penghapus", "3. Pulpen", "4. Penggaris", "1. Buku"];
+// daftarAlatTulis.sort();
+// while (index!=daftarAlatTulis.length) {
+//     const element = daftarAlatTulis[index];
+//     console.log(element);
+//     index++;
+// }
+
+// // soal 4
+// console.log('Soal 4');
+// let s = "";
+// for (let i=1; i<=7;i++){
+//     for(let j=i;j<=7;j++){
+//         s+="*";
+//     }
+//     s+="\n";
+// }
+// console.log(s);
+
+// // soal 5
+// console.log('Soal 5');
+// var jenisKelamin = "L";
+// var nama = "Adi Wirya";
+// if (jenisKelamin == 'L') {
+//     console.log("Bapak "+nama);
+// } else {
+//     console.log("Ibu "+nama);
+// }
