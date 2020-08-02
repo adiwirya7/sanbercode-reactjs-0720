6f126c0188f6bb63167ead4e6f3b69dcfcdc8b63
adi wirya <50317150+adiwirya7@users.noreply.github.com>
2020-08-03 00:10:30 +0700

    Pkn 2 Tgs 5

--- Pekan 2/Tugas Hari Ke-5/test-app/src/App.js
+++ Pekan 2/Tugas Hari Ke-5/test-app/src/App.js
@@ -4,11 +4,47 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-        <h1>Hello World</h1>
-
-  
+    <div className="App" style={{alignContent:"center"}}>
+      <div class="container" style={{width:"50%",border:2,borderColor:"black",borderStyle:"solid",borderRadius:20,textAlign:"left"}}>
+        <form>
+            <h1 style={{textAlign:"center"}}>Form Pembelian Buah</h1>
+            <table style={{width: "70%", padding: "2%", marginLeft: "40px"}}>
+              <tr>
+               <td><strong>Nama Pelanggan </strong></td>
+               <td><input type="text"></input></td>
+              </tr>
+              <tr>
+              <td><strong>&nbsp;</strong></td>
+              </tr>
+              <tr>
+              <td><strong></strong></td>
+              <td> <input type="checkbox"></input>Semangka</td>
+              </tr>
+              <tr>
+              <td><strong></strong></td>
+              <td> <input type="checkbox"></input>Jeruk</td>
+              </tr>
+              <tr>
+              <td><strong></strong></td>
+              <td> <input type="checkbox"></input>Nanas</td>
+              </tr>
+              <tr>
+              <td><strong></strong></td>
+              <td> <input type="checkbox"></input>Salak</td>
+              </tr>
+              <tr>
+              <td><strong>Daftar Item</strong></td>
+              <td> <input type="checkbox"></input>Anggur</td>
+              </tr>
+              <tr>
+              <td><strong></strong></td>
+              </tr>
+              <tr>
+                <td><button style={{borderRadius:10,backgroundColor:"white"}}>Kirim</button></td>
+              </tr>
+            </table>
+        </form>
+      </div>
     </div>
   );
 }
